453b72b940813632fd403187577af49fa0f8bb79
Betina <102986677+BeMarGra@users.noreply.github.com>
2023-08-07 22:29:22 -0300

    aplicando el filtro en los botones

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-// ---variables
+// ---- elementos del html
 let $tarjeta = document.getElementById('tarjeta');
+let $mujerBoton = document.getElementById('mujeres');
+let $hombreBoton = document.getElementById('hombres');
+let $sinGeneroBoton = document.getElementById('sin');
+let $desconocidoBoton = document.getElementById('desconocido');
+let $todosBoton = document.getElementById('todos');
 
 
+//---variables 
+
+let listaPorGenero ;
+let personajes = [];
+let resultado ;
+let resultadoUno ;
+let resultadoDos ;
+let resultadoTres ;
+let resultadoCuatro ;
 
 //---fetch
 
@@ -10,14 +24,14 @@ fetch('https://rickandmortyapi.com/api/character')
     return datos.json();
 })
 .then((datos)=>{
-   let personajes = datos.results;
+   personajes = datos.results;
     mostrar(personajes);
 });
 
 function mostrar(array){
+    $tarjeta.innerHTML = '';
 for(let i = 0; i < array.length; i++){
-    $tarjeta.innerHTML +=   `<div id="tarjeta">
-                                <div class="card">
+    $tarjeta.innerHTML +=   `<div class="card">
                                     <div class="contenedorImg">
                                         <img class="imgPersonaje" src=${array[i].image}>
                                     </div>
@@ -28,7 +42,102 @@ for(let i = 0; i < array.length; i++){
                                     <p>Estado: ${array[i].status}</p>
                                     <p>Origen: ${array[i].origin.name}</p>
                                     <p>Locación: ${array[i].location.name}</p>
-                                </div>
+                        
                             </div>`
 }
+console.log(personajes);
 }
+
+// ---- FILTROS---------
+
+function filtrarTodos(){
+    $mujerBoton.classList.remove('botonFiltroApretado');
+    $mujerBoton.classList.add('filtros');
+    $hombreBoton.classList.remove('botonFiltroApretado');
+    $hombreBoton.classList.add('filtros');
+    $sinGeneroBoton.classList.remove('botonFiltroApretado');
+    $sinGeneroBoton.classList.add('filtros');
+    $desconocidoBoton.classList.remove('botonFiltroApretado');
+    $desconocidoBoton.classList.add('filtros');
+    $todosBoton.classList.remove('filtros');
+    $todosBoton.classList.add('botonFiltroApretado');
+    mostrar(personajes);
+}  
+
+function filtrarMujeres(){
+    $mujerBoton.classList.remove('filtros');
+    $mujerBoton.classList.add('botonFiltroApretado');
+    $hombreBoton.classList.remove('botonFiltroApretado');
+    $hombreBoton.classList.add('filtros');
+    $sinGeneroBoton.classList.remove('botonFiltroApretado');
+    $sinGeneroBoton.classList.add('filtros');
+    $desconocidoBoton.classList.remove('botonFiltroApretado');
+    $desconocidoBoton.classList.add('filtros');
+    $todosBoton.classList.remove('botonFiltroApretado');
+    $todosBoton.classList.add('filtros');
+    resultadoCuatro = personajes.filter((personaje)=>{
+       return personaje.gender === 'Female';
+    })
+mostrar(resultadoCuatro);
+}   
+
+function filtrarHombres(){
+    $mujerBoton.classList.remove('botonFiltroApretado');
+    $mujerBoton.classList.add('filtros');
+    $hombreBoton.classList.remove('filtros');
+    $hombreBoton.classList.add('botonFiltroApretado');
+    $sinGeneroBoton.classList.remove('botonFiltroApretado');
+    $sinGeneroBoton.classList.add('filtros');
+    $desconocidoBoton.classList.remove('botonFiltroApretado');
+    $desconocidoBoton.classList.add('filtros');
+    $todosBoton.classList.remove('botonFiltroApretado');
+    $todosBoton.classList.add('filtros');
+    resultadoUno = personajes.filter((personaje)=>{
+       return personaje.gender === 'Male';
+    })
+mostrar(resultadoUno);
+}   
+
+function filtrarSinGenero(){
+    $mujerBoton.classList.remove('botonFiltroApretado');
+    $mujerBoton.classList.add('filtros');
+    $hombreBoton.classList.remove('botonFiltroApretado');
+    $hombreBoton.classList.add('filtros');
+    $sinGeneroBoton.classList.remove('filtros');
+    $sinGeneroBoton.classList.add('botonFiltroApretado');
+    $desconocidoBoton.classList.remove('botonFiltroApretado');
+    $desconocidoBoton.classList.add('filtros');
+    $todosBoton.classList.remove('botonFiltroApretado');
+    $todosBoton.classList.add('filtros');
+    resultadoDos = personajes.filter((personaje)=>{
+       return personaje.gender === 'genderless';
+    })
+mostrar(resultadoDos);
+}   
+
+function filtrarDesconocido(){
+    $mujerBoton.classList.remove('botonFiltroApretado');
+    $mujerBoton.classList.add('filtros');
+    $hombreBoton.classList.remove('botonFiltroApretado');
+    $hombreBoton.classList.add('filtros');
+    $sinGeneroBoton.classList.remove('botonFiltroApretado');
+    $sinGeneroBoton.classList.add('filtros');
+    $desconocidoBoton.classList.remove('filtros');
+    $desconocidoBoton.classList.add('botonFiltroApretado');
+    $todosBoton.classList.remove('botonFiltroApretado');
+    $todosBoton.classList.add('filtros');
+    resultadoTres = personajes.filter((personaje)=>{
+       return personaje.gender === 'unknown';
+    })
+mostrar(resultadoTres);
+}   
+
+ 
+
+//------eventos-----
+
+$mujerBoton.addEventListener('click',filtrarMujeres)
+$hombreBoton.addEventListener('click',filtrarHombres)
+$sinGeneroBoton.addEventListener('click',filtrarSinGenero)
+$desconocidoBoton.addEventListener('click',filtrarDesconocido)
+$todosBoton.addEventListener('click',filtrarTodos)
